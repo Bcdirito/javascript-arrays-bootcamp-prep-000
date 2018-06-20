260f251f194cb9fd64eba8717914331b2a668522
bdirito91@gmail.com <bdirito91@gmail.com>
2018-06-20 16:21:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var chocolateBars = [
   'skittles'
   ];
 function addElementToBeginningOfArray(chocolateBars, name){
-  chocolateBars = [`${name}`,...chocolateBars];
+  morechocolateBars = [`${name}`,...chocolateBars];
 }
 function destructivelyAddElementToBeginningOfArray(chocolateBars, name) {
   chocolateBars.unshift(`${name}`);
